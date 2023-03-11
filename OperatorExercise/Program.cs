1dbf96a90b268db074a3ed5b061d069c64970b24
Jiger Dhillon <jigersingh29@gmail.com>
2023-03-10 19:19:20 -0500

    initial commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           int a = 17;
+            int b = 4;
+
+            int div = a / b;
+            int mod = a % b;
+
+
+
+            Console.WriteLine($"{a}/{b} is {div} remainder {mod}");
+    
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            var r = 20;
+            var pi = Math.PI;
+            var AreaOfCircle = CalculateArea(20);
+
+            
+
+            Console.WriteLine($"The area of a circle with a radius of 20 is {AreaOfCircle}");
         }
+        public static double CalculateArea(double radius)
+    }
+    {
+        return Math.PI * (radius * radius);
     }
 }
